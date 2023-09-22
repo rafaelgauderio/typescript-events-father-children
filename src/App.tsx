91931d714b4b8022f52c4896bbac0b9bbe69ed24
Rafael de Luca <deluca1712@gmail.com>
2023-09-22 17:17:38 -0300

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <h2>Projeto React tutorial sobre eventos entre componentes pai e filho</h2>
+
+  )
+}
+
+export default App
